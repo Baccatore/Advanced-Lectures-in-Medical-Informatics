99242efb6d814d42958d1279eaf0dd258b1b728f
Baccatore <yuichiro.suga.5@gmail.com>
2018-05-08 21:12:42 +0900

    Update function to calc normal vector

--- ch4/Q0402.py
+++ ch4/Q0402.py
@@ -2,30 +2,9 @@
 # Author    : Yuichiro SUGA
 # Email     : yuichiro.suga@centraliens-lille.org
 # Created   : 2018-05-08 PM 06:35:00
-# Modified  : 
+# Modified  : Tue May  8 21:10:19 JST 2018
 # Mini quiz 2 
 #************************************************************
-import numpy as np
 
 def getNormalVec(x, y):
-    #if x  > 0 and y == 0:
-    #    nx, ny = 0, 1
-    #elif x == 0 and y > 0:
-    #    nx, ny = -1, 0
-    #elif x < 0 and y == 0:
-    #    nx, ny = 0, -1
-    #elif x == 0 and y < 0:
-    #    nx, ny = 1, 0
-    #elif x == 0 and y == 0:
-    #    nx, ny = 0, 0
-    #else:
-    if x == 0:
-        x = 1e-15
-    if y == 0:
-        y = 1e-15
-    tan = x / y
-    tan = - 1 / tan
-    theta = np.arctan(tan)
-    nx, ny = np.sin(theta), np.cos(theta)
-    
-    return (nx, ny)
+    return (y, -x)
