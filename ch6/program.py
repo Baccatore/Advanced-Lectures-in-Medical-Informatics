7dff0b3f07bfa3c65686cdc24627ee41512cd4e7
Baccatore <yuichiro.suga.5@gmail.com>
2018-05-22 18:34:59 +0900

    Start chapter 6

---
+++ ch6/program.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#coding:utf8
+#**************************Q0601.py**************************
+# Author    : Yuichiro SUGA
+# Email     : yuichiro.suga@centraliens-lille.org
+# Created   : 2018-05-22 PM 06:25:27
+# Modified  : 
+# fNIRS data processing
+#************************************************************
